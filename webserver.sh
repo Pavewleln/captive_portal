9e2b17cc97a6c07d1554a4616c18c53f1d0be9b0
pavel <kulikovps2004@gmail.com>
2024-05-11 16:15:49 +0300

    added change config.js

--- webserver.sh
+++ webserver.sh
@@ -25,16 +25,18 @@ fi
 # Get IP address
 ip_address=$(hostname -I | cut -d' ' -f1)
 
-# Copy html folder to /var/www/html
-sudo rm -rf /var/www/html
-sudo cp -r ./portal/html /var/www/html
-
 # Change values in .env file
 sed -i "s|WEB_URL=http://localhost:80|WEB_URL=http://$ip_address:80|" ~/captive_portal/portal/server/.env
 sed -i "s|REDIRECT_OAUTH_URL=http://localhost:4000|REDIRECT_OAUTH_URL=http://$ip_address:4000|" ~/captive_portal/portal/server/.env
 sed -i "s|BACKEND_URL=http://localhost:4000|BACKEND_URL=http://$ip_address:4000|" ~/captive_portal/portal/server/.env
 sed -i "s|POSTGRESQL_DATABASE=portal|POSTGRESQL_DATABASE=$db|" ~/captive_portal/portal/server/.env
 sed -i "s|POSTGRESQL_PASSWORD=portalpass|POSTGRESQL_PASSWORD=$paswd|" ~/captive_portal/portal/server/.env
+sed -i "s|const url = 'http://localhost:4000'|const url = 'http://$ip_address:4000'|" ~/captive_portal/portal/html/js/utils/config.js
+
+
+# Copy html folder to /var/www/html
+sudo rm -rf /var/www/html
+sudo cp -r ./portal/html /var/www/html
 
 # Install dependencies and start the server
 cd ~/captive_portal/portal/server
